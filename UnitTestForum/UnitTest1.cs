863c71517b5765e53ec9b71059e15dc7b554763d
i447092 <i447092@fhict.nl>
2020-12-07 04:07:48 +0100

    First commit

---
+++ UnitTestForum/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
